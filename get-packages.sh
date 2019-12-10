05b717904b1a361808793bd19c1b76ad3fe200ef
Jan Christoph Uhde <Jan@UhdeJC.com>
2019-12-10 10:31:57 +0100

    update get-packages for latest release

--- get-packages.sh
+++ get-packages.sh
@@ -10,13 +10,13 @@ ferr() {
 }
 
 ##please modify this variables
-user="$USER"
+user="${user:-$USER}"
 server="${server:-c9}"
 destination="${destination:-$HOME/release-test-packages}"
 os="${os:-Linux}" #Linux, MacOSX or Windows
 ext="${ext:-deb}"
-version="${version:-3.5.2-1}"
-old_version="${old_version:-3.5.1-1}"
+version="${version:-3.6.0~rc.1-1}"
+old_version="${old_version:-3.5.2-1}"
 
 if [[ $os == "MacOSX" ]]; then
     ext="dmg"
@@ -29,6 +29,7 @@ elif [[ $os == "Windows" ]]; then
 fi
 
 mkdir -p $destination || ferr "could not create folder ${destination}"
+#ssh $user@$server ls -lisah /mnt/buildfiles/stage2/${version:0:3}/packages/Community/$os/arangodb*
 
 for edition in Enterprise Community; do
     suffix=""
