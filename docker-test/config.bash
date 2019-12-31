3c2d548be21afe0a45fef9f63a2bec8c30dbf389
Jan Christoph Uhde <Jan@UhdeJC.com>
2019-12-31 14:17:36 +0100

    add jwt / auth support

--- docker-test/config.bash
+++ docker-test/config.bash
@@ -6,5 +6,8 @@ fi
 
 host_ip="127.0.0.1"
 bind_ip="0.0.0.0"
+password=""
+secret_dir="$HOME"
+secret_file="$secret_dir/jwtSecret"
 image="arangodb/enterprise-preview:3.6.0"
-enterpise_key="${enterpise_key:-${ARANGO_LICENSE_KEY}}"
+enterpise_key="${enterprise_key:-${ARANGO_LICENSE_KEY}}"
